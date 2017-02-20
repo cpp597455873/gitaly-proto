1ff4e045321d8e145b74abffe2ee1499aa23f792
Ahmad Sherif <me@ahmadsherif.com>
2017-02-20 16:04:55 +0200

    Add Commit service with CommitDiff RPC
    Closes gitlab-org/gitaly#80

--- ruby/lib/gitaly/gitaly_pb.rb
+++ ruby/lib/gitaly/gitaly_pb.rb
@@ -18,6 +18,21 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
   end
   add_message "gitaly.PostReceiveResponse" do
   end
+  add_message "gitaly.CommitDiffRequest" do
+    optional :repository, :message, 1, "gitaly.Repository"
+    optional :left_commit_id, :string, 2
+    optional :right_commit_id, :string, 3
+  end
+  add_message "gitaly.CommitDiffResponse" do
+    optional :from_path, :bytes, 1
+    optional :to_path, :bytes, 2
+    optional :from_id, :string, 3
+    optional :to_id, :string, 4
+    optional :old_mode, :int32, 5
+    optional :new_mode, :int32, 6
+    optional :binary, :bool, 7
+    repeated :raw_chunks, :bytes, 8
+  end
 end
 
 module Gitaly
@@ -26,4 +41,6 @@ module Gitaly
   Repository = Google::Protobuf::DescriptorPool.generated_pool.lookup("gitaly.Repository").msgclass
   PostReceiveRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("gitaly.PostReceiveRequest").msgclass
   PostReceiveResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("gitaly.PostReceiveResponse").msgclass
+  CommitDiffRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("gitaly.CommitDiffRequest").msgclass
+  CommitDiffResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("gitaly.CommitDiffResponse").msgclass
 end
