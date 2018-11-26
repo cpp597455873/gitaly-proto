b5d72e17761e42aefc326dbedf18914257b0946e
Nick Thomas <nick@gitlab.com>
2018-11-26 16:55:42 +0000

    Add an RPC to apply a BFG object map to a repository

--- go/gitalypb/shared.pb.go
+++ go/gitalypb/shared.pb.go
@@ -37,7 +37,7 @@ type Repository struct {
 func (m *Repository) Reset()                    { *m = Repository{} }
 func (m *Repository) String() string            { return proto.CompactTextString(m) }
 func (*Repository) ProtoMessage()               {}
-func (*Repository) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{0} }
+func (*Repository) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{0} }
 
 func (m *Repository) GetStorageName() string {
 	if m != nil {
@@ -98,7 +98,7 @@ type GitCommit struct {
 func (m *GitCommit) Reset()                    { *m = GitCommit{} }
 func (m *GitCommit) String() string            { return proto.CompactTextString(m) }
 func (*GitCommit) ProtoMessage()               {}
-func (*GitCommit) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{1} }
+func (*GitCommit) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{1} }
 
 func (m *GitCommit) GetId() string {
 	if m != nil {
@@ -158,7 +158,7 @@ type CommitAuthor struct {
 func (m *CommitAuthor) Reset()                    { *m = CommitAuthor{} }
 func (m *CommitAuthor) String() string            { return proto.CompactTextString(m) }
 func (*CommitAuthor) ProtoMessage()               {}
-func (*CommitAuthor) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{2} }
+func (*CommitAuthor) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{2} }
 
 func (m *CommitAuthor) GetName() []byte {
 	if m != nil {
@@ -188,7 +188,7 @@ type ExitStatus struct {
 func (m *ExitStatus) Reset()                    { *m = ExitStatus{} }
 func (m *ExitStatus) String() string            { return proto.CompactTextString(m) }
 func (*ExitStatus) ProtoMessage()               {}
-func (*ExitStatus) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{3} }
+func (*ExitStatus) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{3} }
 
 func (m *ExitStatus) GetValue() int32 {
 	if m != nil {
@@ -206,7 +206,7 @@ type Branch struct {
 func (m *Branch) Reset()                    { *m = Branch{} }
 func (m *Branch) String() string            { return proto.CompactTextString(m) }
 func (*Branch) ProtoMessage()               {}
-func (*Branch) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{4} }
+func (*Branch) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{4} }
 
 func (m *Branch) GetName() []byte {
 	if m != nil {
@@ -237,7 +237,7 @@ type Tag struct {
 func (m *Tag) Reset()                    { *m = Tag{} }
 func (m *Tag) String() string            { return proto.CompactTextString(m) }
 func (*Tag) ProtoMessage()               {}
-func (*Tag) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{5} }
+func (*Tag) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{5} }
 
 func (m *Tag) GetName() []byte {
 	if m != nil {
@@ -291,7 +291,7 @@ type User struct {
 func (m *User) Reset()                    { *m = User{} }
 func (m *User) String() string            { return proto.CompactTextString(m) }
 func (*User) ProtoMessage()               {}
-func (*User) Descriptor() ([]byte, []int) { return fileDescriptor12, []int{6} }
+func (*User) Descriptor() ([]byte, []int) { return fileDescriptor13, []int{6} }
 
 func (m *User) GetGlId() string {
 	if m != nil {
@@ -331,9 +331,9 @@ func init() {
 	proto.RegisterType((*User)(nil), "gitaly.User")
 }
 
-func init() { proto.RegisterFile("shared.proto", fileDescriptor12) }
+func init() { proto.RegisterFile("shared.proto", fileDescriptor13) }
 
-var fileDescriptor12 = []byte{
+var fileDescriptor13 = []byte{
 	// 594 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x53, 0xc1, 0x6e, 0xd4, 0x3c,
 	0x10, 0xd6, 0x66, 0xb3, 0xdb, 0xee, 0x6c, 0xfa, 0xff, 0x60, 0x7a, 0x88, 0x8a, 0xaa, 0x96, 0x20,
